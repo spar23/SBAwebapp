afd313843ae1656df710b32acc2801aaf0ad3548
Shar Parsapour <sparsapour@yahoo.com>
2022-06-04 15:45:01 -0700

    last update

--- js/validation.js
+++ js/validation.js
@@ -5,10 +5,9 @@ window.onload = init;
 
 // DOM ELEMENTS
 const elements = {
-    // we don't want to get the values immediately, so wrap them all in a function that can be executed on init
+   
     collectDOM: function(){
         this.formElement = document.getElementById("contact-form");
-        // this.formMessageElement = document.getElementById("form-error");
         this.name = document.getElementById("nme");
         this.message = document.getElementById("msg");
         this.email = document.getElementById("email");
@@ -19,18 +18,14 @@ function init(){
     // initialize dom elements
     elements.collectDOM();
     // attach listener to the form submit button
-    elements.formElement.addEventListener("submit", validateForm);
-    
+    elements.formElement.addEventListener("submit", validateForm);   
     
 }
-
 
 
 function validateForm(event){
     event.preventDefault();
     console.log("Form submission button clicked");
-    // console.log(event.currentTarget);
-    // get form data from <input> tags using .value
     const nameText = elements.name.value;
     const messageText = elements.message.value;
     const emailText = elements.email.value;
@@ -41,8 +36,6 @@ function validateForm(event){
     } else if (nameText === ""){
         alert("Please input a name.")
         console.log(nameText);
-        // update the DOM with an error message
-        // elements.formMessageElement.innerText = "Name must not be blank.";
 
     } else if(messageText === ""){
         alert("Please input a message.")
